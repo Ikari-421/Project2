bc3be3452e6bc4bf601b03390fc123325b28807f
Ikari_421 <maximilien.auclair@gmail.com>
2024-02-06 10:02:58 +0100

    Fixing bug Kpi page

--- pages/3_Analyse et KPIs.py
+++ pages/3_Analyse et KPIs.py
@@ -84,182 +84,13 @@ with st.sidebar:
 
     st.image("images/Logo-limouzen_wide.png")
 
-#-----------------
-#    MAIN
-#-----------------
 
-with st.container():
-    st.markdown("<h1 style='text-align: center; color:##2c2e58; margin-top:-80px;'>📈 Analyse et KPIs</h1>", unsafe_allow_html=True)
-    # st.markdown("<h2 style='color:#006699;margin-bottom:-40px;'>Les Graphiques:</h2>", unsafe_allow_html=True)
-    st.header("", divider='rainbow')
-
-with st.container(border=True):
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Analyse secteur", "Finances", "Production companies",
-                                            "Acteurs / directeur / écrivant","Genre"
-                                            ])
-    with tab1:
-        st.markdown("""<h3 class='title-tab'>Analyse secteur</h3>
-                    """, unsafe_allow_html=True)
-
-    with tab2:
-        st.markdown("<h3 class='title-tab'>Finances</h3>", unsafe_allow_html=True)
-        # hm.charts_core("finance_charts", filtered_df)
-
-        from plotly.subplots import make_subplots
-        import plotly.graph_objects as go
-
-        filtered_df['Profit'] = filtered_df['revenue'] - filtered_df['budget']
-
-        top_10_profitable_movies = filtered_df.sort_values(by='revenue', ascending=False).head(10)
-
-        fig1 = px.bar(top_10_profitable_movies, x='originalTitle', y='Profit',
-                    title='Top 10 Films by Profit',
-                    labels={'originalTitle': 'Film Title', 'Profit': 'Profit'},
-                    color='Profit')
-
-        fig1.update_layout(xaxis_tickangle=30,
-            xaxis_title='Film Title',
-            yaxis_title='Profit')
-
-        top_10_by_revenue = filtered_df.sort_values(by='revenue', ascending=False).head(10)
-
-        fig2 = make_subplots(specs=[[{"secondary_y": True}]])
-
-        fig2.add_trace(
-            go.Bar(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['revenue'], name='Revenue'),
-            secondary_y=False,)
-
-        fig2.add_trace(
-            go.Bar(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['budget'], name='Budget'),
-            secondary_y=False,)
-
-        fig2.add_trace(
-            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['Profit'], name='Profit', mode='lines+markers'),
-            secondary_y=True,)
-
-        fig2.update_layout(xaxis_tickangle=30,
-            title_text='Top 10 Films by Revenue, Budget, and Profit',
-            barmode='group',)
-
-        fig2.update_xaxes(title_text='Film Title')
-        fig2.update_yaxes(title_text='Revenue / Budget', secondary_y=False)
-        fig2.update_yaxes(title_text='Profit', secondary_y=True)
-
-
-
-        top_10_by_budget = filtered_df.sort_values(by='budget', ascending=False).iloc[1:11]
-
-
-        fig3 = px.bar(top_10_by_budget, x='originalTitle', y=['budget', 'Profit'],
-                            title='Top 10 Films by Budget and Their Profits',
-                            labels={'originalTitle': 'Film Title', 'value': 'Amount', 'variable': 'Type'})
-
-        fig3.update_layout(xaxis_tickangle=30,
-            xaxis_title='Film Title',
-            yaxis_title='Amount')
-
-
-
-        top_10_by_revenue = filtered_df.sort_values(by='revenue', ascending=False).head(10)
-
-        fig4 = px.bar(top_10_by_revenue, x='originalTitle', y=['revenue', 'Profit'],
-                            title='Top 10 Films by Revenue and Their Profits',
-                            labels={'originalTitle': 'Film Title', 'value': 'Amount', 'variable': 'Type'})
-
-        fig4.update_layout(xaxis_tickangle=30,
-            xaxis_title='Film Title',
-            yaxis_title='Amount')
-
-
-        top_10_by_revenue = filtered_df.sort_values(by='revenue', ascending=False).head(10)
-
-
-        fig5 = make_subplots(specs=[[{"secondary_y": True}]])
-        fig5.add_trace(
-            go.Bar(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['revenue'], name='Revenue'),
-            secondary_y=False)
-
-
-        fig5.add_trace(
-            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['Profit'], name='Profit', mode='lines+markers'),
-            secondary_y=True)
-
-
-        fig5.add_trace(
-            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['numVotes'], name='Number of Votes', mode='lines+markers'),
-            secondary_y=True,)
-
-        fig5.add_trace(
-            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['averageRating'], name='Average Rating', mode='lines+markers'),
-            secondary_y=True,)
-
-
-        fig5.update_layout(xaxis_tickangle=30,
-            title_text='Top 10 Films by Revenue, Profit, Number of Votes, and Average Rating',
-            barmode='group',)
-
-        fig5.update_xaxes(title_text='Film Title')
-        fig5.update_yaxes(title_text='Revenue', secondary_y=False)
-        fig5.update_yaxes(title_text='Profit / Votes / Rating', secondary_y=True)
-
-
-        with st.expander("Select Graphs to Display"):
-            show_graph_1 = st.checkbox('Top 10 Films by Profit', True)
-            show_graph_2 = st.checkbox('Top 10 Films by Revenue, Budget, and Profit', True)
-            show_graph_3 = st.checkbox('Top 10 Films by Budget and Their Profits', True)
-            show_graph_4 = st.checkbox('Top 10 Films by Revenue and Their Profits', True)
-            show_graph_5 = st.checkbox('Top 10 Films by Revenue, Profit, Number of Votes, and Average Rating', True)
-
-        graphs_selected = sum([show_graph_1, show_graph_2, show_graph_3, show_graph_4, show_graph_5])
-
-        def create_graph_column(fig, col):
-            with col:
-                st.plotly_chart(fig, use_container_width=True)
-
-        if graphs_selected == 1:
-            if show_graph_1:
-                st.plotly_chart(fig1, use_container_width=True)
-            elif show_graph_2:
-                st.plotly_chart(fig2, use_container_width=True)
-            elif show_graph_3:
-                st.plotly_chart(fig3, use_container_width=True)
-            elif show_graph_4:
-                st.plotly_chart(fig4, use_container_width=True)
-            elif show_graph_5:
-                st.plotly_chart(fig5, use_container_width=True)
-
-        elif graphs_selected == 2:
-            cols = st.columns(2)
-            graphs = [show_graph_1, show_graph_2, show_graph_3, show_graph_4, show_graph_5]
-            figures = [fig1, fig2, fig3, fig4, fig5]
-            displayed = 0
-            for i, graph in enumerate(graphs):
-                if graph:
-                    create_graph_column(figures[i], cols[displayed % 2])
-                    displayed += 1
-
-        else:
-            first_row = st.columns(2)
-            second_row = st.columns(2)
-            third_row = None
-            graphs = [show_graph_1, show_graph_2, show_graph_3, show_graph_4, show_graph_5]
-            figures = [fig1, fig2, fig3, fig4, fig5]
-            displayed = 0
-            for i, graph in enumerate(graphs):
-                if graph:
-                    if displayed < 2:
-                        create_graph_column(figures[i], first_row[displayed % 2])
-                    elif displayed < 4:
-                        create_graph_column(figures[i], second_row[displayed % 2])
-                    else:
-                        if not third_row:
-                            third_row = st.columns(2)
-                        create_graph_column(figures[i], third_row[displayed % 2])
-                    displayed += 1
-
-    with tab3:
-        st.markdown("<h3 class='title-tab'>Production companies</h3>", unsafe_allow_html=True)
-        # hm.production_charts(filtered_df)
+#
+# ALL FUNCTIONS 
+#
+                    
+                    
+def production_charts(filtered_df):
 
         background_color = "#f9f9f9"
         text_color = "DarkSlateGrey"
@@ -460,12 +291,160 @@ with st.container(border=True):
                         create_graph_column(figures[i], third_row[displayed % 2])
                     displayed += 1
 
+def finance_charts(filtered_df):
+        from plotly.subplots import make_subplots
+        import plotly.graph_objects as go
 
-    with tab4:
-        st.markdown("<h3 class='title-tab'>Acteurs / directeur / écrivant</h3>", unsafe_allow_html=True)
-        # hm.actors_charts(csv_movies, title_principals, name_basics)
-        movies_filtered = csv_movies
+        filtered_df['Profit'] = filtered_df['revenue'] - filtered_df['budget']
 
+        top_10_profitable_movies = filtered_df.sort_values(by='revenue', ascending=False).head(10)
+
+        fig1 = px.bar(top_10_profitable_movies, x='originalTitle', y='Profit',
+                    title='Top 10 Films by Profit',
+                    labels={'originalTitle': 'Film Title', 'Profit': 'Profit'},
+                    color='Profit')
+
+        fig1.update_layout(xaxis_tickangle=30,
+            xaxis_title='Film Title',
+            yaxis_title='Profit')
+
+        top_10_by_revenue = filtered_df.sort_values(by='revenue', ascending=False).head(10)
+
+        fig2 = make_subplots(specs=[[{"secondary_y": True}]])
+
+        fig2.add_trace(
+            go.Bar(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['revenue'], name='Revenue'),
+            secondary_y=False,)
+
+        fig2.add_trace(
+            go.Bar(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['budget'], name='Budget'),
+            secondary_y=False,)
+
+        fig2.add_trace(
+            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['Profit'], name='Profit', mode='lines+markers'),
+            secondary_y=True,)
+
+        fig2.update_layout(xaxis_tickangle=30,
+            title_text='Top 10 Films by Revenue, Budget, and Profit',
+            barmode='group',)
+
+        fig2.update_xaxes(title_text='Film Title')
+        fig2.update_yaxes(title_text='Revenue / Budget', secondary_y=False)
+        fig2.update_yaxes(title_text='Profit', secondary_y=True)
+
+
+
+        top_10_by_budget = filtered_df.sort_values(by='budget', ascending=False).iloc[1:11]
+
+
+        fig3 = px.bar(top_10_by_budget, x='originalTitle', y=['budget', 'Profit'],
+                            title='Top 10 Films by Budget and Their Profits',
+                            labels={'originalTitle': 'Film Title', 'value': 'Amount', 'variable': 'Type'})
+
+        fig3.update_layout(xaxis_tickangle=30,
+            xaxis_title='Film Title',
+            yaxis_title='Amount')
+
+
+
+        top_10_by_revenue = filtered_df.sort_values(by='revenue', ascending=False).head(10)
+
+        fig4 = px.bar(top_10_by_revenue, x='originalTitle', y=['revenue', 'Profit'],
+                            title='Top 10 Films by Revenue and Their Profits',
+                            labels={'originalTitle': 'Film Title', 'value': 'Amount', 'variable': 'Type'})
+
+        fig4.update_layout(xaxis_tickangle=30,
+            xaxis_title='Film Title',
+            yaxis_title='Amount')
+
+
+        top_10_by_revenue = filtered_df.sort_values(by='revenue', ascending=False).head(10)
+
+
+        fig5 = make_subplots(specs=[[{"secondary_y": True}]])
+        fig5.add_trace(
+            go.Bar(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['revenue'], name='Revenue'),
+            secondary_y=False)
+
+
+        fig5.add_trace(
+            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['Profit'], name='Profit', mode='lines+markers'),
+            secondary_y=True)
+
+
+        fig5.add_trace(
+            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['numVotes'], name='Number of Votes', mode='lines+markers'),
+            secondary_y=True,)
+
+        fig5.add_trace(
+            go.Scatter(x=top_10_by_revenue['originalTitle'], y=top_10_by_revenue['averageRating'], name='Average Rating', mode='lines+markers'),
+            secondary_y=True,)
+
+
+        fig5.update_layout(xaxis_tickangle=30,
+            title_text='Top 10 Films by Revenue, Profit, Number of Votes, and Average Rating',
+            barmode='group',)
+
+        fig5.update_xaxes(title_text='Film Title')
+        fig5.update_yaxes(title_text='Revenue', secondary_y=False)
+        fig5.update_yaxes(title_text='Profit / Votes / Rating', secondary_y=True)
+
+
+        with st.expander("Select Graphs to Display"):
+            show_graph_1 = st.checkbox('Top 10 Films by Profit', True)
+            show_graph_2 = st.checkbox('Top 10 Films by Revenue, Budget, and Profit', True)
+            show_graph_3 = st.checkbox('Top 10 Films by Budget and Their Profits', True)
+            show_graph_4 = st.checkbox('Top 10 Films by Revenue and Their Profits', True)
+            show_graph_5 = st.checkbox('Top 10 Films by Revenue, Profit, Number of Votes, and Average Rating', True)
+
+        graphs_selected = sum([show_graph_1, show_graph_2, show_graph_3, show_graph_4, show_graph_5])
+
+        def create_graph_column(fig, col):
+            with col:
+                st.plotly_chart(fig, use_container_width=True)
+
+        if graphs_selected == 1:
+            if show_graph_1:
+                st.plotly_chart(fig1, use_container_width=True)
+            elif show_graph_2:
+                st.plotly_chart(fig2, use_container_width=True)
+            elif show_graph_3:
+                st.plotly_chart(fig3, use_container_width=True)
+            elif show_graph_4:
+                st.plotly_chart(fig4, use_container_width=True)
+            elif show_graph_5:
+                st.plotly_chart(fig5, use_container_width=True)
+
+        elif graphs_selected == 2:
+            cols = st.columns(2)
+            graphs = [show_graph_1, show_graph_2, show_graph_3, show_graph_4, show_graph_5]
+            figures = [fig1, fig2, fig3, fig4, fig5]
+            displayed = 0
+            for i, graph in enumerate(graphs):
+                if graph:
+                    create_graph_column(figures[i], cols[displayed % 2])
+                    displayed += 1
+
+        else:
+            first_row = st.columns(2)
+            second_row = st.columns(2)
+            third_row = None
+            graphs = [show_graph_1, show_graph_2, show_graph_3, show_graph_4, show_graph_5]
+            figures = [fig1, fig2, fig3, fig4, fig5]
+            displayed = 0
+            for i, graph in enumerate(graphs):
+                if graph:
+                    if displayed < 2:
+                        create_graph_column(figures[i], first_row[displayed % 2])
+                    elif displayed < 4:
+                        create_graph_column(figures[i], second_row[displayed % 2])
+                    else:
+                        if not third_row:
+                            third_row = st.columns(2)
+                        create_graph_column(figures[i], third_row[displayed % 2])
+                    displayed += 1
+
+def actors_charts(movies_filtered, title_principals, name_basics):
         movies_filtered1 = movies_filtered[['tconst', 'averageRating', 'originalTitle', 'numVotes', 'ranking_1']]
         title_principals1 = title_principals[['tconst', 'nconst', 'category']]
         name_basics1 = name_basics[['nconst', 'primaryName']]
@@ -607,10 +586,7 @@ with st.container(border=True):
                         create_graph_column(figures[i], second_row[displayed % 2])
                     displayed += 1
 
-
-    with tab5:
-        st.markdown("<h3 class='title-tab'>Genres</h3>", unsafe_allow_html=True)
-        # hm.genres_charts(filtered_df)
+def genres_charts(filtered_df):
 
         filtered_df['genres'] = filtered_df['genres'].astype(str)
         filtered_df['genres'] = filtered_df['genres'].str.split(',')
@@ -692,6 +668,7 @@ with st.container(border=True):
             legend=dict(orientation="v", y=1, x=0, xanchor='left', yanchor='top')
         )
 
+
         # same with the percentage of number of films per each genre through the decades
 
         genre_counts_over_decades = filtered_genres.groupby(['Decade', 'genres']).size().reset_index(name='Film Count')
@@ -758,3 +735,37 @@ with st.container(border=True):
                         create_graph_column(figures[i], second_row[displayed % 2])
                     displayed += 1
 
+
+
+#-----------------
+#    MAIN
+#-----------------
+
+with st.container():
+    st.markdown("<h1 style='text-align: center; color:##2c2e58; margin-top:-80px;'>📈 Analyse et KPIs</h1>", unsafe_allow_html=True)
+    # st.markdown("<h2 style='color:#006699;margin-bottom:-40px;'>Les Graphiques:</h2>", unsafe_allow_html=True)
+    st.header("", divider='rainbow')
+
+with st.container(border=True):
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Analyse secteur", "Finances", "Production companies",
+                                            "Acteurs / directeur / écrivant","Genre"
+                                            ])
+    with tab1:
+        st.markdown("""<h3 class='title-tab'>Analyse secteur</h3>
+                    """, unsafe_allow_html=True)
+
+    with tab2:
+        st.markdown("<h3 class='title-tab'>Finances</h3>", unsafe_allow_html=True)
+        finance_charts(filtered_df)
+
+    with tab3:
+        st.markdown("<h3 class='title-tab'>Production companies</h3>", unsafe_allow_html=True)
+        production_charts(filtered_df)
+
+    with tab4:
+        st.markdown("<h3 class='title-tab'>Acteurs / directeur / écrivant</h3>", unsafe_allow_html=True)
+        actors_charts(csv_movies, title_principals, name_basics)
+
+    with tab5:
+        st.markdown("<h3 class='title-tab'>Genres</h3>", unsafe_allow_html=True)
+        genres_charts(filtered_df)
